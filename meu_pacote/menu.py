b7791e9275c60e733e95a665ca990659dde946e3
Vitor Barbosa <143558773+VitorHBS@users.noreply.github.com>
2025-02-11 13:23:57 -0300

    Creating asset folder Inserting all assets Creating the Manu: background, sounds and fonts Creating the const file

--- meu_pacote/menu.py
+++ meu_pacote/menu.py
@@ -1,9 +1,40 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+from tkinter.font import Font
+import pygame
+from meu_pacote.Const import WIN_WIDTH, WIN_HEIGHT, COLOR_ORANGE, MENU_OPTION, COLOR_WHITE
+
+
 class Menu:
-    def __init__(self):
-        self.window = None
+    def __init__(self, window):
+        self.window = window
+        self.surf = pygame.image.load('./assets/MenuBg.png')
+        self.rect = self.surf.get_rect(left=0, top=0)
 
     def run(self, ):
-        pass
+        pygame.mixer_music.load('./assets/Menu.mp3')
+        pygame.mixer_music.play(-1)
+
+        while True:
+            self.window.blit(source=self.surf, dest=self.rect)
+            self.menu_text(50, "Mountain", COLOR_ORANGE, ((WIN_WIDTH / 2), 70))
+            self.menu_text(50, "Shooter", COLOR_ORANGE, ((WIN_WIDTH / 2), 120))
+            
+            for i in range(len(MENU_OPTION)):
+                self.menu_text(20, MENU_OPTION[i], COLOR_WHITE, ((WIN_WIDTH / 2), 200 + 25 * i))
+
+            pygame.display.flip()
+            
+            # Check for all events
+            for event in pygame.event.get():
+               if event.type == pygame.QUIT:
+                   print('Quitting...')
+                   pygame.quit()  # Close Window
+                   quit() # end pygame
+    
+    def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
+        text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
+        text_surf: pygame.Surface = text_font.render(text, True, text_color).convert_alpha()
+        text_rect: pygame.Rect = text_surf.get_rect(center=text_center_pos)
+        self.window.blit(source=text_surf, dest=text_rect)
